3a97c4ef227126b638ceabf6e89fa16b51d4b867
nester7x <nester.letopisets@gmail.com>
2021-05-04 12:11:46 +0300

    commit 2 projects

---
+++ Andrey/squarespace/js/main.js
@@ -0,0 +1,10 @@
+$(function (){
+    $('.exclusive__slider').slick({
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 3000
+    });
+});
