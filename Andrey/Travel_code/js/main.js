657b3a6b46ebfceb2e44ed03837ce7763e0de660
nester7x <nester.letopisets@gmail.com>
2021-08-18 14:40:24 +0300

    Travel_code commit v3.0

--- Andrey/Travel_code/js/main.js
+++ Andrey/Travel_code/js/main.js
@@ -1,21 +1,20 @@
 $(function () {
     /*header-form*/
 
-    const search = document.querySelector('.search');
-    const inputSearch = document.querySelector('.header__input-search');
-    const cancel = document.querySelector('.header__search-cancel');
-    const searchBlock = document.querySelector('.header__search');
+    const searchCancel = document.querySelector('.header__search-cancel');
+    const searchBtn = document.querySelector('.header__input-search');
+    const searchForm = document.querySelector('.header__search');
 
-    search.onclick = function () {
-        cancel.classList.remove('display-none')
-        inputSearch.classList.remove('display-none')
-        searchBlock.classList.add('search-center')
+    searchBtn.onclick = function () {
+        searchCancel.classList.add('active');
+        searchBtn.classList.add('active');
+        searchForm.classList.add('active');
     };
 
-    cancel.onclick = function () {
-        cancel.classList.add('display-none')
-        inputSearch.classList.add('display-none')
-        searchBlock.classList.remove('search-center')
+    searchCancel.onclick = function () {
+        searchCancel.classList.remove('active');
+        searchBtn.classList.remove('active');
+        searchForm.classList.remove('active');
     };
 
     /*------------------------------------*/
@@ -38,33 +37,9 @@ $(function () {
 
     /*menu appearing and disappearing*/
 
-    const pageWidth = document.documentElement.clientWidth;
-    const menu = document.querySelector('.menu');
-    const menuBtn = document.querySelector('.menu-btn');
-    const main = document.querySelector('.main');
-
-    function pageWidthCheck() {
-        if (pageWidth <= 768) {
-            menuBtn.classList.remove('display-none')
-        }
-        else {
-            menuBtn.classList.add('display-none')
-        }
-    }
-
-    pageWidthCheck();
-
-    menuBtn.addEventListener('click', function () {
-        if (menu.classList.contains('up-side-menu')) {
-            menu.classList.remove('up-side-menu')
-            main.classList.remove('margin-top')
-            main.classList.add('margin-top-off')
-        }
-        else {
-            menu.classList.add('up-side-menu')
-            main.classList.add('margin-top')
-            main.classList.remove('margin-top-off')
-        }
+    $('.menu-btn').click(function (event) {
+       $('.menu').toggleClass('show');
+       $('.menu-btn').toggleClass('show');
     });
 
     /*------------------------------------*/
@@ -103,7 +78,7 @@ $(function () {
 
     for (let item of slickArrow) {
         item.addEventListener('click', function () {
-                line.classList.add('display-none')
+            line.classList.remove('liner')
         })
     }
 
